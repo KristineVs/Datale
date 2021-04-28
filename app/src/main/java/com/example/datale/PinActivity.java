30235449f05424c262579c65db1d504339e79815
Luka Dragar <D20124481@mytudublin.ie>
2021-04-28 14:20:09 +0100

    User now sets the pin on first launch

--- app/src/main/java/com/example/datale/PinActivity.java
+++ app/src/main/java/com/example/datale/PinActivity.java
@@ -3,7 +3,9 @@ package com.example.datale;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
@@ -12,17 +14,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PinActivity extends AppCompatActivity {
 
     private static final String TAG = "PinActivity";
     EditText enter_mpin;
+    TextView textViewPrompt;
     ImageView i1, i2, i3, i4;
     Button[] numbers = new Button[10];
-    String pin;
 
-    String pinInput = "";
+    String pinSHA256;
+    String userId;
+
+    String currentPinInput = "";
+
+    SharedPreferences preferences;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,27 +51,38 @@ public class PinActivity extends AppCompatActivity {
         numbers[8] = findViewById(R.id.button_eight);
         numbers[9] = findViewById(R.id.button_nine);
 
-        pin = "1234"; //after, database blabla....
+        i1 = findViewById(R.id.imageview_circle1);
+        i2 = findViewById(R.id.imageview_circle2);
+        i3 = findViewById(R.id.imageview_circle3);
+        i4 = findViewById(R.id.imageview_circle4);
 
-        i1 = (ImageView) findViewById(R.id.imageview_circle1);
-        i2 = (ImageView) findViewById(R.id.imageview_circle2);
-        i3 = (ImageView) findViewById(R.id.imageview_circle3);
-        i4 = (ImageView) findViewById(R.id.imageview_circle4);
-
+        textViewPrompt = findViewById(R.id.text_view_pin_prompt);
         enter_mpin = (EditText) findViewById(R.id.editText_enter_mpin);
         enter_mpin.requestFocus();
         enter_mpin.setInputType(InputType.TYPE_CLASS_NUMBER);
         enter_mpin.setFocusableInTouchMode(true);
 
-        for (int i = 0; i <numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             final int count = i;
             numbers[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    pinInput += Integer.toString(count);
-                    enter_mpin.setText(pinInput);
+                    currentPinInput += Integer.toString(count);
+                    enter_mpin.setText(currentPinInput);
                 }
             });
+        }
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        pinSHA256 = preferences.getString("saved_pin", "");
+
+        // first time opening app
+        if (pinSHA256.equals("")) {
+            textViewPrompt.setText("Set PIN");
+
+            // generate userId from date
+            SimpleDateFormat currentDatetime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            userId = sha256(currentDatetime.format(new Date()));
         }
 
         enter_mpin.addTextChangedListener(new TextWatcher() {
@@ -76,9 +99,20 @@ public class PinActivity extends AppCompatActivity {
                 Log.d(TAG, "onKey: screen key pressed");
                 switch (s.length()) {
                     case 4:
-                        i4.setImageResource(R.drawable.circle2);
-                        String input = s.toString();
-                        Login(input);
+                        try {
+                            i4.setImageResource(R.drawable.circle2);
+                            if (pinSHA256.equals("")) {
+                                // encrypt pin and save it with userId
+                                SharedPreferences.Editor editor = preferences.edit();
+                                editor.putString("saved_pin", sha256(s.toString()));
+                                editor.apply();
+
+                                Login("");
+                            } else
+                                Login(sha256(s.toString()));
+                        } catch (Exception e) {
+
+                        }
                         break;
                     case 3:
                         i4.setImageResource(R.drawable.circle);
@@ -98,30 +132,41 @@ public class PinActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
-    public void Login(String p)
-    {
-        if(p.equals(pin))
-        {
+
+    public String sha256(final String base) {
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void Login(String p) {
+        if (p.equals(pinSHA256) || p.equals("")) {
             Intent i;
             i = new Intent(this, MainActivity.class);
             startActivity(i);
             finish();
-        }
-        else{
-            Toast toast = Toast.makeText(this.getApplicationContext(),"Wrong Pin.", Toast.LENGTH_SHORT);
-          
+        } else {
+            Toast toast = Toast.makeText(this.getApplicationContext(), "Wrong Pin.", Toast.LENGTH_SHORT);
+
             toast.show();
             i1.setImageResource(R.drawable.circle);
             i2.setImageResource(R.drawable.circle);
             i3.setImageResource(R.drawable.circle);
             i4.setImageResource(R.drawable.circle);
-            pinInput = "";
-            enter_mpin.setText(pinInput);
-
-
+            currentPinInput = "";
+            enter_mpin.setText(currentPinInput);
         }
     }
 }
